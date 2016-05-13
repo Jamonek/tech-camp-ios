689c9b1f95bba7f2f5057861d2b659ad0c968925
Jamone Kelly <xg.monie089@gmail.com>
2016-05-12 21:21:30 -0400

    - Add new icons for tab bar - Add chameleon global theming in AppDelegate - Adjust tab bar in storyboard

--- Podfile
+++ Podfile
@@ -2,12 +2,12 @@ platform :ios, '8.0'
 use_frameworks!
 
 target 'Teen Summit App' do
-	pod 'ChameleonFramework/Swift'
-	pod 'RxSwift',   '~> 2.0'
-  pod 'RxCocoa',   '~> 2.0'
-  pod 'Moya-ObjectMapper/RxSwift'
-  pod 'FontAwesomeKit'
-  pod 'Fabric'
-  pod 'Crashlytics'
+    pod 'ChameleonFramework/Swift'
+    pod 'RxSwift',   '~> 2.0'
+    pod 'RxCocoa',   '~> 2.0'
+    pod 'Moya-ObjectMapper/RxSwift'
+    pod 'FontAwesomeKit'
+    pod 'Fabric'
+    pod 'Crashlytics'
 end
 
--- Teen Summit App/AppDelegate.swift
+++ Teen Summit App/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import ChameleonFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Chameleon.setGlobalThemeUsingPrimaryColor(UIColor(hexString: "315ABC"), withContentStyle: .Contrast)
         return true
     }
 
